9604e820dd1545242e81d9829135f624930f4b4b
Letanir <MrLetanir@gmail.com>
2019-10-10 15:28:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     return (
         <div className="App">
             <Makeup/>
-            <General/>
-            <Fruits/>
+            {/* <General/> */}
+            {/* <Fruits/> */}
         </div>
     );
 }
